c629adb900e9ea38c51188b23080e014b7731860
Joseph Mendezona <joseph_mendezona@hotmail.com>
2024-06-18 01:22:13 +0200

    add cron job logic to api route

--- orders/src/app/api/alpaca/checkpriceatnextinterval/route.ts
+++ orders/src/app/api/alpaca/checkpriceatnextinterval/route.ts
@@ -1,5 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 import { verifySignatureAppRouter } from "@upstash/qstash/nextjs";
+import { type AlpacaCheckLatestPriceAndReverseTradeCronJobParams } from "~/actions/exchanges/alpaca/alpaca.types";
+import { alpacaCheckLatestPriceAndReverseTradeCronJob } from "~/actions/exchanges/alpaca/alpacaCronJobs";
 
 export const dynamic = "force-dynamic";
 
@@ -7,8 +9,15 @@ export const POST = verifySignatureAppRouter(async (request: Request) => {
   console.log("API called - alpaca/checkpriceatnextinterval");
   try {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const data = await request.json();
+    const data =
+      (await request.json()) satisfies AlpacaCheckLatestPriceAndReverseTradeCronJobParams;
     console.log("checkpriceatnextinterval: data", data);
+    await alpacaCheckLatestPriceAndReverseTradeCronJob({
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+      tradingViewSymbol: data.tradingViewSymbol,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+      buyAlert: data.buyAlert,
+    } as AlpacaCheckLatestPriceAndReverseTradeCronJobParams);
     return new Response(JSON.stringify({ success: true }), {
       status: 200,
       headers: {
