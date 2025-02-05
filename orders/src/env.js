868810240b41cb82ff2f0aa17c19036a1a04829e
mendezona <josephmendezonawork@gmail.com>
2025-02-05 22:11:35 +0100

    add environment variable ci check

--- orders/src/env.js
+++ orders/src/env.js
@@ -7,10 +7,24 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
+    ALPACA_LIVE_KEY: z.string(),
+    ALPACA_LIVE_SECRET: z.string(),
+    ALPACA_PAPER_KEY: z.string(),
+    ALPACA_PAPER_SECRET: z.string(),
+    ALPACA_TRADINGVIEW_SYMBOLS: z.string(),
+    ALPACA_TRADINGVIEW_INVERSE_PAIRS: z.string(),
     POSTGRES_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    DEVELOPMENT_MODE: z.boolean(),
+    FINANCIAL_YEAR_PERIOD: z.string(),
+    QSTASH_TOKEN: z.string(),
+    QSTASH_URL: z.string(),
+    QSTASH_NEXT_SIGNING_KEY: z.string(),
+    QSTASH_CURRENT_SIGNING_KEY: z.string(),
+    SENTRY_AUTH_TOKEN: z.string(),
+    TRADINGVIEW_AUTH_TOKEN: z.string(),
   },
 
   /**
@@ -27,9 +41,23 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
+    ALPACA_LIVE_KEY: process.env.ALPACA_LIVE_KEY,
+    ALPACA_LIVE_SECRET: process.env.ALPACA_LIVE_SECRET,
+    ALPACA_PAPER_KEY: process.env.ALPACA_PAPER_KEY,
+    ALPACA_PAPER_SECRET: process.env.ALPACA_PAPER_SECRET,
+    ALPACA_TRADINGVIEW_SYMBOLS: process.env.ALPACA_TRADINGVIEW_SYMBOLS,
+    ALPACA_TRADINGVIEW_INVERSE_PAIRS:
+      process.env.ALPACA_TRADINGVIEW_INVERSE_PAIRS,
     POSTGRES_URL: process.env.POSTGRES_URL,
     NODE_ENV: process.env.NODE_ENV,
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    DEVELOPMENT_MODE: process.env.DEVELOPMENT_MODE,
+    FINANCIAL_YEAR_PERIOD: process.env.FINANCIAL_YEAR_PERIOD,
+    QSTASH_TOKEN: process.env.QSTASH_TOKEN,
+    QSTASH_URL: process.env.QSTASH_URL,
+    QSTASH_NEXT_SIGNING_KEY: process.env.QSTASH_NEXT_SIGNING_KEY,
+    QSTASH_CURRENT_SIGNING_KEY: process.env.QSTASH_CURRENT_SIGNING_KEY,
+    SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN,
+    TRADINGVIEW_AUTH_TOKEN: process.env.TRADINGVIEW_AUTH_TOKEN,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
